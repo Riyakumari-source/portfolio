554a0628bc3d5880a61d768f514cb6813ef855dd
Riyakumari-Source <riyakumarii0207@gmail.com>
2026-07-05 16:14:52 +0530

    fix: render optimized tech stack physics canvas on mobile

--- src/sections/TechStack.tsx
+++ src/sections/TechStack.tsx
@@ -139,53 +139,51 @@ export const TechStack = () => {
         </div>
       </div>
 
-      {isDesktopMode && (
-        <div className="techstack-canvas-container">
-          <Canvas
-            eventSource={wrapperRef as unknown as React.MutableRefObject<HTMLElement>}
-            shadows
-            gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
-            camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
-            onCreated={(sceneState) => {
-              sceneState.gl.toneMappingExposure = 1.5;
-            }}
-            className="tech-canvas"
-          >
-            <ambientLight intensity={1} />
-            <spotLight
-              position={[20, 20, 25]}
-              penumbra={1}
-              angle={0.2}
-              color="white"
-              castShadow
-              shadow-mapSize={[512, 512]}
+      <div className="techstack-canvas-container">
+        <Canvas
+          eventSource={wrapperRef as unknown as React.MutableRefObject<HTMLElement>}
+          shadows
+          gl={{ alpha: true, stencil: false, depth: false, antialias: false }}
+          camera={{ position: [0, 0, 20], fov: 32.5, near: 1, far: 100 }}
+          onCreated={(sceneState) => {
+            sceneState.gl.toneMappingExposure = 1.5;
+          }}
+          className="tech-canvas"
+        >
+          <ambientLight intensity={1} />
+          <spotLight
+            position={[20, 20, 25]}
+            penumbra={1}
+            angle={0.2}
+            color="white"
+            castShadow
+            shadow-mapSize={[512, 512]}
+          />
+          <directionalLight position={[0, 5, -4]} intensity={2} />
+          <Physics gravity={[0, 0, 0]}>
+            <KinematicMouseCollider
+              isActive={physicsEngineActiveFlag}
+              isHovering={isHoveringInteractive}
             />
-            <directionalLight position={[0, 5, -4]} intensity={2} />
-            <Physics gravity={[0, 0, 0]}>
-              <KinematicMouseCollider
+            {(isDesktopMode ? sphereInstances : sphereInstances.slice(0, 18)).map((sphereProps, sphereIdx) => (
+              <PhysicsSphere
+                key={`physics-ball-${sphereIdx}`}
+                {...sphereProps}
+                material={ballMaterialsList[Math.floor(Math.random() * ballMaterialsList.length)]}
                 isActive={physicsEngineActiveFlag}
-                isHovering={isHoveringInteractive}
               />
-              {sphereInstances.map((sphereProps, sphereIdx) => (
-                <PhysicsSphere
-                  key={`physics-ball-${sphereIdx}`}
-                  {...sphereProps}
-                  material={ballMaterialsList[Math.floor(Math.random() * ballMaterialsList.length)]}
-                  isActive={physicsEngineActiveFlag}
-                />
-              ))}
-            </Physics>
-            <Environment
-              preset="studio"
-              environmentIntensity={0.5}
-              environmentRotation={[0, 4, 2]}
-            />
-            <EffectComposer enableNormalPass={false}>
-              <N8AO color="#20000a" aoRadius={2} intensity={1.15} />
-            </EffectComposer>
-          </Canvas>
-        </div>
-      )}
+            ))}
+          </Physics>
+          <Environment
+            preset="studio"
+            environmentIntensity={0.5}
+            environmentRotation={[0, 4, 2]}
+          />
+          <EffectComposer enableNormalPass={false}>
+            <N8AO color="#20000a" aoRadius={2} intensity={1.15} />
+          </EffectComposer>
+        </Canvas>
+      </div>
     </div>
   );
 };
